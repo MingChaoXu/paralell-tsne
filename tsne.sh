11c76e6ed07647f07ba5467536e35429041a7801
MingChaoXu <xumingchao_2013@163.com>
2019-05-27 10:57:30 +0800

    sr test

--- tsne.sh
+++ tsne.sh
@@ -11,9 +11,11 @@ fi
 echo $list_param
 
 
-python -u tsne.py \
- --root '' \
- $list_param \
+CUDA_VISIBLE_DEVICES=1,2,3 python -u tsne.py \
+ --root /lustre/home/mcxu/experiments/super-resolution/SRGAN-reg-eng \
+ --sr_matri ../tsne/v2-1,0.1/netD_epoch_4_1_iter_180000.pth_sr.npy \
+ --hr_matri ../tsne/v2-1,0.1/netD_epoch_4_1_iter_180000.pth_hr.npy \
+ --hr_restore_matri ../tsne/v2-1,0.1/netD_epoch_4_1_iter_180000.pth_hr_restore.npy \
  --out result \
 #  --tsne_input feature_person_trainv1.npy \
 #  --target target_person_trainv1.npy \
