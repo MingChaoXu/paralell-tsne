832241e7e4f802cb683d3917e4f7d4566066af3b
MingChaoXu <xumingchao_2013@163.com>
2019-08-18 18:16:40 +0800

    setup

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ echo "run: python setup.py sdist bdist_wheel"
 python setup.py sdist bdist_wheel
 echo "run: test pypi"
 twine upload --repository pypitest dist/*
-# echo "run: upload!"
-# twine upload dist/*
+echo "run: upload!"
+twine upload dist/*
